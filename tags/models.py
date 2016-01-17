8617b34b8170645cc4d3e4a944b110b4ca8fe293
Irina Pchelintseva <lipka@84.201.164.117-vpna.dhcp.yndx.net>
2016-01-17 15:41:21 +0300

    proper names for tags and genres

--- tags/models.py
+++ tags/models.py
@@ -2,13 +2,20 @@ from __future__ import unicode_literals
 
 from django.db import models
 from hvad import models as hvad
+from django.utils.encoding import python_2_unicode_compatible
 
-
+@python_2_unicode_compatible
 class Tag(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 
 class Genre(hvad.TranslatableModel):
     translations = hvad.TranslatedFields(
             title=models.CharField(max_length=200)
     )
+
+    def __str__(self):
+        return self.title
